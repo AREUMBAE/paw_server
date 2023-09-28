95faf30795334bc98ff6114ef30458859ff10856
김상현 <gimsanghyeon@gimsanghyeons-MacBook-Air.local>
2023-09-29 02:59:31 +0900

    초기

---
+++ project/src/test/java/paw_walk/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package paw_walk.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
